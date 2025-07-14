6e4519f704f268c606ba43c1006ffae06a72d3aa
cara-odoo <cara@odoo.com>
2025-07-14 13:10:54 -0400

    [ADD] odoovation_event: add event models, views, data, .gitignore

---
+++ odoovation_event/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Odoovation WW - Events",
+    "description": """
+        - To do
+    """,
+    "category": "Custom Development",
+    "version": "1.1.0",
+    "author": "Odoo Development Services",
+    "maintainer": "Odoo Development Services",
+    "website": "https://www.odoo.com",
+    "license": "OPL-1",
+    "depends": ["event"],
+    "data": [],
+}
